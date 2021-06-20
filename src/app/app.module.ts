90e431294458188786e230cc345d7e6caa6eda59
Parikshit5504 <desaip932@gmail.com>
2021-06-20 04:45:50 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { InfoComponent } from './info/info.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatMenuModule} from '@angular/material/menu';
+import { ChildcompComponent } from './childcomp/childcomp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserdetailsComponent,
+    InfoComponent,
+    ChildcompComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/childcomp/childcomp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-childcomp',
+  templateUrl: './childcomp.component.html',
+  styleUrls: ['./childcomp.component.css']
+})
+export class ChildcompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  @Input() public names: any;
+}
